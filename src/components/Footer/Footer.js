1ebea7f686667488d4fe105fc356af7ab0fa9c0f
amilanila@gmail.com <amilanila@gmail.com>
2017-03-04 13:17:11 +1100

    Responsive width of bar chart (need more fixing though)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 const baseStyle = {
 	'text-align': 'center',
-	'margin-top': -35,
 	'background-color': 'black',
 	'font-color': '#000000',
+	'vertical-align': 'middel',
 	'height': 100
+};
+
+const copyRightTextStyle = {
+	'position': 'relative',
+	'top': '50%' 
 };
 
 const Footer = () => {
 	return (
 		<div style={ {...baseStyle} }>
-			Copyright@ ANS Consulting Pty Ltd
+			<span style={ {...copyRightTextStyle} }>Copyright@ ANS Consulting Pty Ltd</span>
 		</div>
 	);
 };
--- src/components/ScoreBarChart/ScoreBarChart.js
+++ src/components/ScoreBarChart/ScoreBarChart.js
@@ -2,13 +2,44 @@ import React, { Component, PropTypes } from 'react';
 import { BarChart } from 'react-easy-chart';
 
 const baseStyles = {
-	padding: 30
 };
 
 export class ScoreBarChart extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			height: window.innerHeight,
+			width: window.innerWidth
+		}
+
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+	}
+
+	componentDidMount() {
+		this.updateWindowDimensions();
+	  	window.addEventListener('resize', this.updateWindowDimensions.bind(this));
+	}
+
+	componentWillUnmount() {
+	  	window.removeEventListener('resize', this.updateWindowDimensions.bind(this));
+	}
+
+	updateWindowDimensions() {
+	  	this.setState({ width: window.innerWidth, height: window.innerHeight });
+	}
+
 	render() {
 		let graphData = this.props.graphData;
+		let maxY = 0;
+		if (!!graphData && !!graphData.length) {
+			for (let data of graphData) {
+				if (maxY < data.y) {
+					maxY = data.y;
+				}
+			}
+		}
 		
 		return (
 			<div style={ {...baseStyles} }>
@@ -18,10 +49,10 @@ export class ScoreBarChart extends Component {
 					    axes
 					    grid
 					    colorBars
-					    height={250}
-					    width={650}
+					    height={200}
+					    width={this.state.width}
 					    xDomainRange={[1, 15]}
-						yDomainRange={[0, 20]}
+						yDomainRange={[0, (maxY + 15)]}
 						interpolate={'cardinal'}
 						y2Type="linear"
 					    data={graphData}
